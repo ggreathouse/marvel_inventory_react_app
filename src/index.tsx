d68261993684dd2dfcc431dde1c63cd6ffa8d3d1
Garrett Greathouse <garrett.greathouse@hotmail.com>
2022-05-02 14:24:24 -0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Dashboard, SignIn } from './components';
+import './styles.css'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path = '/' element={<Home title={'Marvel Inventory'} />} />
+        <Route path = '/dashboard' element={ <Dashboard />} />
+        <Route path = '/signin' element={<SignIn />} />
+      </Routes>
+    
+    </Router>
   </React.StrictMode>
 );
 
