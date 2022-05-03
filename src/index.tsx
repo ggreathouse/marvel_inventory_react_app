69d69d955a7ba64e36e9c56125f50eb5112da82e
Garrett Greathouse <garrett.greathouse@hotmail.com>
2022-05-03 16:58:36 -0700

    updated dashboard

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,24 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home, Dashboard, SignIn } from './components';
 import './styles.css'
-
+import { theme } from './Theme/themes'
+import { ThemeProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path = '/' element={<Home title={'Marvel Inventory'} />} />
-        <Route path = '/dashboard' element={ <Dashboard />} />
-        <Route path = '/signin' element={<SignIn />} />
-      </Routes>
-    
-    </Router>
+    <ThemeProvider theme = {theme}>
+      <Router>
+        <Routes>
+          <Route path = '/' element={<Home title={'Marvel Inventory'} />} />
+          <Route path = '/dashboard' element={ <Dashboard />} />
+          <Route path = '/signin' element={<SignIn />} />
+        </Routes>
+      
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
